2204f87adf0d9787071c12f97b133d1d92da5b00
Your Name <58438984+Anton-Rogov@users.noreply.github.com>
2019-12-08 00:42:35 +0300

    Добавлен JS!

--- students/Rogov_Anton/Project/script.js
+++ students/Rogov_Anton/Project/script.js
@@ -1,1 +1,19 @@
 "use strict";
+class Ingrit {
+    constructor(nam) {
+        this.Name = this._inputRadio(name);
+    }
+_inputRadio (domElementName){
+    return document.querySelector(`input[name="${domElementName}"]: checked`).value
+}
+inputCheck (domElementName){
+    return document.querySelector(`input[name="${domElementName}"]: checked`).value
+}
+let burger =[];
+let order = document.querySelector('# order');
+order.addEventListener(click, addIngrid);
+
+function addIngrid() {
+    let newIngrit = new Ingrit('name');
+    burger.push(newIngrit)
+}
